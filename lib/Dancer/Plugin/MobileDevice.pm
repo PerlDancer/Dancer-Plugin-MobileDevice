16b4a142051c7cfad958099d90296c59e28b39c6
Yanick Champoux <yanick@babyl.dyndns.org>
2013-09-28 17:40:00 -0400

    Fix links

--- lib/Dancer/Plugin/MobileDevice.pm
+++ lib/Dancer/Plugin/MobileDevice.pm
@@ -105,10 +105,6 @@ You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dancer-Plugin-MobileDevice>
-
 =item * AnnoCPAN: Annotated CPAN documentation
 
 L<http://annocpan.org/dist/Dancer-Plugin-MobileDevice>
